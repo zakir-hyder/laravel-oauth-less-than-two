5f81fabc5f5f6a3c0afcf09b57c57efb921c97af
Zakir Hyder <zakir.hyder@gmail.com>
2014-05-10 11:47:35 +0600

    unnecessary file delted

--- credential.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'twitter_client_key'     => $_ENV['twitter_client_key'],
-  'twitter_client_secret' => $_ENV['twitter_client_secret'],
-  'twitter_base_url' => 'https://api.twitter.com/oauth/',
-);
